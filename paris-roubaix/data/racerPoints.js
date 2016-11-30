9ad0fce5e0ff92c83396789cbb998372bd757ed8
Riley Hoonan <hoonr188@newschool.edu>
2016-11-30 20:47:44 +0000

    fixed ptsTot bug, added cutoffs for top 10% and top 25 riders

--- paris-roubaix/data/racerPoints.js
+++ paris-roubaix/data/racerPoints.js
@@ -1,12 +1,23 @@
 var fs = require('fs');
 var request = require('request');
+var d3 = require('d3');
+var async = require('async');
 
 var pointsUCI = [500,400,325,275,225,175,150,125,100,85,70,60,50,40,35,30,30,30,30,30,20,20,20,20,20,20,20,20,20,20,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,5,5,5,5,5,3,3,3,3,3];
 // ranking points based on: http://www.uci.ch/mm/Document/News/Rulesandregulation/17/73/59/2-ROA-20161108-E_English.PDF Page 60
 
 var races = JSON.parse(fs.readFileSync('parisRoubaix-fullv2.json'));
 
+// all riders has every racer ever to race in Paris Roubaix
 var allRiders = {};
+// cutoff1 has every racer ever to rank top 60 or higher (score more than 0)
+var cutoff1 = {};
+// cutoff2 is the top 10%
+var cutoff2 = {};
+// cutoff3 are the top 10 riders
+var cutoff3 = {};
+
+var avg;
 
 //use the pointsUCI array to find the points scored
 function ptsCalc(rank) {
@@ -19,6 +30,7 @@ function ptsCalc(rank) {
 
 //loops through rider to calc cumulative points
 function ptsSuminator(rider) {
+    
     var total = 0;
     for (var year in rider.years) {
         total += parseFloat(rider.years[year].pts);
@@ -27,11 +39,11 @@ function ptsSuminator(rider) {
     
 }
 
-for ( var cyclist in races) {
-    
-    var riderName = races[cyclist].name;
-    var year = races[cyclist].year;
-    var rank = races[cyclist].rank;
+async.each(races, function(cyclist, callback) {
+        
+    var riderName = cyclist.name; 
+    var year = cyclist.year;
+    var rank = cyclist.rank;
     
     //set an index in lower case with all spaces removed
     var riderIndex = riderName.toLowerCase().replace(/\s/g, '');
@@ -56,15 +68,125 @@ for ( var cyclist in races) {
         var newResults = allRiders[riderIndex].years[year];
             newResults.rank = rank;
             newResults.pts = ptsCalc(rank);
-            newResults.ptsTot = ptsSuminator(allRiders[riderIndex]) + newResults.pts;
+            newResults.ptsTot = ptsSuminator(allRiders[riderIndex]); // + newResults.pts
             
     }
+        
+}, function(err) {
+    console.log(err);
+});
+
+// for ( var cyclist in races) {
+    
+// }
+
+getStats();
+cutoffs();
+
+function getTotalPoints(rider) {
+    
+    // make years into array
+    var years = d3.entries(rider.years);
+    // get the cumulative total (held in the last year of each)
+    var lastYearRaced = years.length-1;
+    var ptsTot = years[lastYearRaced].value.ptsTot;
+    
+    return ptsTot;
+}
+
+//calc mean avg
+function avgPts() {
+    
+    var allRidersTot = 0;
+    var allPtsTot = 0;
+    
+    for (var rider in allRiders) {
+        allPtsTot += getTotalPoints(allRiders[rider]);
+        allRidersTot++;
+    }
+    
+    var avg = allPtsTot/allRidersTot;
+    
+    return [avg, allRidersTot];
     
 }
 
+function getStats() {
+    
+    var sum = 0;
+    
+    var avg = avgPts()[0];
+    var allRidersTot = avgPts()[1];
+    var maxPts = 0;
+    var maxPtsRider;
+    
+    console.log('Mean UCI points per Rider: '+avg);
+    
+    for (var rider in allRiders) {
+        var ptsTot = getTotalPoints(allRiders[rider]);
+        sum += Math.pow((ptsTot-avg), 2);
+        
+        if (ptsTot > maxPts) { 
+            maxPts = ptsTot;
+            maxPtsRider = allRiders[rider].name;
+        }
+        
+    }
+    
+    var stdDev = Math.sqrt(sum/allRidersTot);
+    
+    console.log('Standard Deviation UCI points: '+stdDev);
+    console.log('Most Points Scored: '+maxPts+' '+maxPtsRider);
+    
+}
+
+function cutoffs() {
+    
+    var allRidersTot = 0;
+    var cutoff1Tot = 0;
+    var cutoff2Tot = 0;
+    var cutoff3Tot = 0;
+    
+    for (var rider in allRiders) {
+        
+        allRidersTot++;
+        
+        var ptsTot = getTotalPoints(allRiders[rider]);
+        
+        
+        if (ptsTot > 0) {
+            cutoff1Tot++;
+            cutoff1[rider] = allRiders[rider];
+        }
+        
+        if (ptsTot > 328) {
+            cutoff2Tot++;
+            cutoff2[rider] = allRiders[rider];
+        }
+        
+        if (ptsTot > 1800) {
+            cutoff3Tot++;
+            cutoff3[rider] = allRiders[rider];
+        }
+    }
+    
+    console.log('CUTOFFS');
+    console.log('Total riders ranked: '+allRidersTot);
+    console.log('Total riders scoring any points: '+cutoff1Tot);
+    console.log('Riders scoring 328+ points (Top 10%): '+cutoff2Tot);   
+    console.log('Top 25 Riders:'); 
+    for (rider in cutoff3) {
+        console.log(cutoff3[rider]);
+    }
+}
 // console.log(allRiders);
 
 	fs.writeFile('byRider.json', JSON.stringify(allRiders), function(err) {
         if (err) {throw err;}
-        console.log("done");
+        console.log("byRider written");
+    });
+    
+    fs.writeFile('top10percent.json', JSON.stringify(cutoff2),function(err) {
+        if (err) {throw err;}
+        console.log("cutoff2 written");
     });
