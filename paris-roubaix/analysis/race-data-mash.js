6349ef77390d60ce035af08eb470751cc8907d86
Riley Hoonan <Riley.Hoonan@gmail.com>
2017-01-21 11:00:19 -0500

    pr v0.6 analysis updates

--- paris-roubaix/analysis/race-data-mash.js
+++ paris-roubaix/analysis/race-data-mash.js
@@ -1,4 +1,4 @@
-// Race Data Mash compiles PCS' data (which is missing several years of finisher times in 
+// Race Data Mash compiles PCS' data (which is missing several years of finisher times in
 // the 20's and 30's) into the more robust BRI data object
 
 // Adds in starter data
@@ -13,7 +13,6 @@ var racesBRI = JSON.parse(fs.readFileSync('races-bri.json'));
 var racesPCS = JSON.parse(fs.readFileSync('races-pcs.json'));
 var startersBox = {};
 
-
 var lastRealTime;
 
 // just the race distances by year
@@ -21,25 +20,31 @@ var races = [];
 
 fs.readFile("starters.csv", "utf8", function(error, startersIn) { // from http://learnjsdata.com/node.html
     startersIn = d3.csvParse(startersIn);
-    
+
+    // loops through ever year in starters in
     for (var yr in startersIn) {
+        //if the number of starters is available, puts that data in startersBox
         if (!isNaN(startersIn[yr].starters)) {
+            // per year
             startersBox[startersIn[yr].year] = startersIn[yr].starters;
+            // marks 'estimated (est)' property of object 'false'
             startersBox[startersIn[yr].year+'est'] = false;
         } else {
+            // makes the number of starters the average
             startersBox[startersIn[yr].year] = '123';
             startersBox[startersIn[yr].year+'est'] = true;
         }
-        console.log(startersBox[startersIn[yr].year]);
+        // console.log(startersBox[startersIn[yr].year]);
     }
 
-    console.log(startersBox);
+    // console.log(startersBox);
 
+    // loops through each racer in racesPCS, gets the year and distance, and pushes it to an array
     for (var racer = 0; racer < racesPCS.length; racer++) {
-        
+
         var thisYear = parseFloat(racesPCS[racer].year);
-        
-        if (thisYear == 1896) { 
+
+        if (thisYear == 1896) {
             var Race = new Object();
             Race.year = parseFloat(racesPCS[racer].year);
             Race.distKm = parseFloat(racesPCS[racer].distance.replace('k',''));
@@ -48,7 +53,7 @@ fs.readFile("starters.csv", "utf8", function(error, startersIn) { // from http:/
             var prev = racer-1;
             var lastYear = parseFloat(racesPCS[prev].year);
         }
-        
+
         if (thisYear != lastYear) {
             // make a new race
             var Race = new Object();
@@ -56,19 +61,19 @@ fs.readFile("starters.csv", "utf8", function(error, startersIn) { // from http:/
             Race.distKm = parseFloat(racesPCS[racer].distance.replace('k',''));
             races.push(Race);
         }
-        
+
     }
-    
+
     //adding 2016, which isn't in the PCS data
     var Race2016 = new Object();
     Race2016.year = 2016;
     Race2016.distKm = 257.5;
     races.push(Race2016);
-    
+
     // console.log(races);
-    
+
     for (var racer in racesBRI) {
-        
+
         // use next closest time for missing data points, and mark 'est' true
         if (racesBRI[racer].time == undefined || racesBRI[racer].time == 'READ-ERR') {
             racesBRI[racer].time = lastRealTime;
@@ -77,10 +82,10 @@ fs.readFile("starters.csv", "utf8", function(error, startersIn) { // from http:/
             lastRealTime = racesBRI[racer].time;
             racesBRI[racer].est = false;
         }
-        
+
         // calculate speed -- for some reason this misses some race winners (but not all)
         for (var race in races) {
-            
+
             if (racesBRI[racer].year == races[race].year) {
                 //get & assign race distance
                 racesBRI[racer].distKm = races[race].distKm;
@@ -89,60 +94,65 @@ fs.readFile("starters.csv", "utf8", function(error, startersIn) { // from http:/
                 break;
             }
         }
-        
+
         // find the number of known finishers where unknown
         if (racesBRI[racer].finishers == 'NA') {
             rankFix(racesBRI[racer].year); // ERROR on race winners introduced here?
         }
-        
+
         //set the number of starters
         racesBRI[racer].starters = startersBox[racesBRI[racer].year];
     }
-    
+
     namesAndCountries();
-    
+
 // console.log(racesBRI);
 
 	fs.writeFile('parisRoubaix-fullv2.json', JSON.stringify(racesBRI), function(err) {
         if (err) {throw err;}
         console.log("done");
     });
- 
+
 }); //end callback to read csv file
 
 
 
- //Names and Countries loops through all of the results and tries to find 
-    //matching racers with the same names, cleans their names, and assigns countries to them
-    //(it probably misses a few racers in the span that PCS has no data for)
+    // Names and Countries loops through all of the results and tries to find
+    // matching racers with the same names, cleans their names, and assigns countries to them
+    // (it misses a few racers without countries)
     function namesAndCountries() {
-        
+
         for (var racerBRI in racesBRI) {
             // 'Eo' Emile van Berendonck
-            
+            // console.log(racesBRI[racerBRI]);
+
             for (var racerPCS in racesPCS) {
-                
-                // test to see if names are a match 
-                // not really sophisticated enough to handle edge cases, like missing first names...
-                var hasFirst = racesBRI[racerBRI].name.toLowerCase().includes(racesPCS[racerPCS].firstName.toLowerCase());
-                var hasLast = racesBRI[racerBRI].name.toLowerCase().includes(racesPCS[racerPCS].lastName.toLowerCase());
-                //kill the ghost of Eo
-                if ( hasFirst && hasLast && racesPCS[racerPCS].lastName != 'Eo') { 
+
+                var fnPCS = racesPCS[racerPCS].firstName.toLowerCase();
+                var lnPCS = racesPCS[racerPCS].lastName.toLowerCase();
+
+                // test to see if names are a match
+                var hasFirst = racesBRI[racerBRI].name.toLowerCase().includes(fnPCS);
+                var hasLast = racesBRI[racerBRI].name.toLowerCase().includes(lnPCS);
+
+                /
+                if ( hasFirst && hasLast && racesPCS[racerPCS].lastName != 'Eo') {
+                    // do a better comparison here
+                    // PROBLEM: Some racers have 3 or 4 names. SOLUTION: Split the names into an array and check each one?
                     racesBRI[racerBRI].name = racesPCS[racerPCS].firstName +' '+ racesPCS[racerPCS].lastName;
                     racesBRI[racerBRI].country = racesPCS[racerPCS].country;
-                    console.log('N&C '+racesBRI[racerBRI].name);
                     break;
                 }
             }
         }
-        
+
     } // end names and countries
-    
+
     // if NA is detected, loop through all racers with this year, find the max rank and assign that to finishers
     function rankFix(year) {
-        
+
         var maxRank = 0;
-        
+
         for (var racer in racesBRI) {
             if (racesBRI[racer].year == year) {
                 if (racesBRI[racer].rank > maxRank) {
@@ -152,11 +162,11 @@ fs.readFile("starters.csv", "utf8", function(error, startersIn) { // from http:/
                 break;
             }
         }
-        
+
         for (racer in racesBRI) {
             if (racesBRI[racer].year == year) {
                 racesBRI[racer].finishers = maxRank;
             }
         }
-        
+
     } //end rankfix
